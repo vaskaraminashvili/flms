26ca65ff6f6638c33159e6a5afeb4d9f24861e31
Vasili Raminashvili <vaska.r02@gmail.com>
2024-07-11 19:09:37 +0400

    finished video 11

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Exports\StudentsExport;
 use App\Filament\Resources\StudentResource\Pages;
 use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Classes;
 use App\Models\Section;
 use App\Models\Student;
 use Filament\Forms;
@@ -21,33 +22,6 @@ class StudentResource extends Resource
     protected static ?string $model = Student::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('class_id')
-                    ->live()
-                    ->relationship('class', 'name'),
-                Forms\Components\Select::make('section_id')
-                    ->label("Section")
-                    ->options(function (Get $get) {
-                        $class_id = $get('class_id');
-                        if ($class_id) {
-                            return Section::where('class_id', $class_id)->pluck('name', 'id');
-                        }
-                    }),
-                Forms\Components\TextInput::make('name')
-                    ->autofocus()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->unique()
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-            ]);
-    }
 
     public static function table(Table $table): Table
     {
@@ -76,7 +50,30 @@ class StudentResource extends Resource
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+
+                Tables\Filters\Filter::make('has_class')
+                    ->label('has class')
+                    ->form([
+                        Forms\Components\Select::make('class_id')
+                            ->label('Filter By Class')
+                            ->placeholder('Filter by Class')
+                            ->live()
+                            ->options(function () {
+                                return Classes::pluck('name', 'id')->toArray();
+                            }),
+                        Forms\Components\Select::make('section_id')
+                            ->label('Filter By Section')
+                            ->placeholder('Filter by Section')
+                            ->options(function (Get $get) {
+                                return Section::where('class_id', $get('class_id'))->pluck('name', 'id')->toArray();
+                            })
+                    ])->query(function ($query, array $data) {
+                        return $query->when($data['class_id'] ?? null, function ($query) use ($data) {
+                            return $query->where('class_id', $data['class_id']);
+                        })->when($data['section_id'] ?? null, function ($query) use ($data) {
+                            return $query->where('section_id', $data['section_id']);
+                        });
+                    })
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -86,11 +83,38 @@ class StudentResource extends Resource
                     Tables\Actions\DeleteBulkAction::make(),
                     Tables\Actions\BulkAction::make('export')
                         ->label('Export to Excel')
-                        ->icon('heroicon-o-rectangle-stack ')
+                        ->icon('heroicon-o-rectangle-stack')
                         ->action(function (Collection $records) {
                             return Excel::download(new StudentsExport($records), 'students.xlsx');
                         })
                 ]),
+            ]);
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('class_id')
+                    ->live()
+                    ->relationship('class', 'name'),
+                Forms\Components\Select::make('section_id')
+                    ->label("Section")
+                    ->options(function (Get $get) {
+                        $class_id = $get('class_id');
+                        if ($class_id) {
+                            return Section::where('class_id', $class_id)->pluck('name', 'id');
+                        }
+                    }),
+                Forms\Components\TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->unique()
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 
